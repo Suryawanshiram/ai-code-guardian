7fe83198ca405f28373b5edaa7166943eaf9c121
nbpawar <netaji.pawar@outlook.com>
2026-02-23 11:28:27 +0530

    feat: implement GitHub integration with repo fetch and PR skeleton using auth token

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 // import { pullRequestRouter } from "./routers/pull-request";
-// import { repositoryRouter } from "./routers/respository";
+import { repositoryRouter } from "./routers/respository";
 // import { reviewRouter } from "./routers/review";
 import { createCallerFactory, createTRPCRouter, publicProcedure } from "./trpc";
 
@@ -7,9 +7,9 @@ export const appRouter = createTRPCRouter({
   health: publicProcedure.query(() => {
     return { status: "ok", timestamps: new Date() };
   }),
-//   repository: repositoryRouter,
-//   pullRequest: pullRequestRouter,
-//   review: reviewRouter,
+  repository: repositoryRouter,
+  //   pullRequest: pullRequestRouter,
+  //   review: reviewRouter,
 });
 
 export type AppRouter = typeof appRouter;
